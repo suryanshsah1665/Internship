2bf813812cbbd0266eb46c9f0bd97be72eb7c51c
Suryanshsah1665 <suryanshsahs@gmail.com>
2026-03-07 23:07:20 +0530

    commit

--- frontend/script.js
+++ frontend/script.js
@@ -207,3 +207,9 @@ async function loadHistory() {
     }
 
 }
+let userId = localStorage.getItem("user_id");
+
+if (!userId) {
+    userId = "user_" + Math.random().toString(36).substring(2,10);
+    localStorage.setItem("user_id", userId);
+}
