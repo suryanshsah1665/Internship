a23f5c07d26256c21355806e29f53d1da970e036
Suryanshsah1665 <suryanshsahs@gmail.com>
2026-03-17 11:53:13 +0530

    commit

--- backend/app.py
+++ backend/app.py
@@ -105,3 +105,8 @@ def get_history():
 # -------------------------------
 if __name__ == "__main__":
     app.run(debug=True)
+
+import os
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
